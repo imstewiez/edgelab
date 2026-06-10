1dbd1c9036d6bfd1e13243aaaea786dfe8d6f086
imstewiez <steven.southan97@gmail.com>
2026-06-10 04:28:09 +0100

    Make installer script path-safe

--- INSTALL_ALL.bat
+++ INSTALL_ALL.bat
@@ -1,5 +1,7 @@
 @echo off
+setlocal
 title CoreEA EdgeLab Install
+cd /d "%~dp0"
 echo Installing engine dependencies...
 cd apps\engine
 if not exist .venv (
@@ -7,7 +9,7 @@ if not exist .venv (
 )
 call .venv\Scripts\activate
 pip install -r requirements.txt
-cd ..\..\apps\web
+cd /d "%~dp0apps\web"
 echo Installing web dependencies...
 npm install
 echo Done.
